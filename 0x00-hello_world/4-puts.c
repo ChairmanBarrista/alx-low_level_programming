cf0a011b9c16145ec6d12f0b7085e6c3f072c130
Chairmman Barrista <barristaamoah@gmail.com>
2023-06-15 09:19:29 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * A C program to print out a statement and return 0
+ */
+int main (void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
