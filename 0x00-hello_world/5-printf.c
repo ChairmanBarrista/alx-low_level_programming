39298bcb9469c2425523a400fe9969c04a97e760
Chairmman Barrista <barristaamoah@gmail.com>
2023-06-15 09:36:32 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - serves as the starting point for program execution using printf
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
