f5310aef0d08cf8f83c1a5bc549c9d0414896bc3
Chairmman Barrista <barristaamoah@gmail.com>
2023-06-15 10:09:12 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - serves as the starting point for program execution
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of long: %d byte(s)\n", sizeof((long));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
+	printf("Size of long int: %d byte(s)\n", sizeof(long int));
+	return (0);
+}
+
