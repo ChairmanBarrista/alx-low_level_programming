6c23348dfece6a795ca16e977266ff8f04a72511
Chairmman Barrista <barristaamoah@gmail.com>
2023-06-16 11:55:13 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print single numbers.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	for (x = 48; x < 58; x++);
+	{
+		putchar(x);
+	}
+	putchar('/n');
+	return (0);
+}
