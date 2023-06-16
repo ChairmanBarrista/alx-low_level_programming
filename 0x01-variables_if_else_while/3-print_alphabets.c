8ff582f43955815fce022e3a25fd0b2181a1eff3
Chairmman Barrista <barristaamoah@gmail.com>
2023-06-16 09:03:03 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Printing the Alphabets.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabets[52] = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+	putchar(alphabets[i]);
+	}
+putchar('\n');
+
+return (0);
+}
