40809c9a276d6133ea45b337b25e61444d990b11
Chairmman Barrista <barristaamoah@gmail.com>
2023-06-16 08:55:04 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char alphabet[26] = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
