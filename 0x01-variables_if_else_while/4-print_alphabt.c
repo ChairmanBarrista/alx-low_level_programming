a206792f9bcdfc2eeae283a4a8200bdfcceacc66
Chairmman Barrista <barristaamoah@gmail.com>
2023-06-16 11:41:50 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - print all alphabets except q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x =97; x < 123; x++)
+	{
+		if (x != 101 && x != 113)
+		{
+			putchar(x);
+		}
+	}
+		putchar('\n');
+		return (0);
+}
