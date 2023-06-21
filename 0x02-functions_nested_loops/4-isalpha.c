f12afc7c0b297198d674412b65a743bc5272f048
Chairmman Barrista <barristaamoah@gmail.com>
2023-06-21 09:45:05 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - alphabetic character
+ * @c: The character to be checked
+ * Return: 1 for alphabetic character or 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+if ((x >= 65; && x <= 97) || (x >= 97 && x <= 122))
+{
+return (1);
+}
+return (0);
+}
