6fd68afaee9d429dee5ceb89da767d2e77debce2
Chairmman Barrista <barristaamoah@gmail.com>
2023-06-21 09:02:58 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,8 +7,8 @@
 
 void print_alphabet(void)
 {
-	char xy;
-	for (xy = 'a'; xy <= 'z'; xy++)
-		_putchar(xy);
-		_putchar('\n');
+char xy;
+for (xy = 'a'; xy <= 'z'; xy++)
+_putchar(xy);
+_putchar('\n');
 }
