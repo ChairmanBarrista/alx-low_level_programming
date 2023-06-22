a5e466a6416c712969a4a419b71350c55c9374ed
Chairmman Barrista <barristaamoah@gmail.com>
2023-06-22 06:21:38 -0700

    Task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - is number positive or negative
+ * 0: number to be checked
+ * Return: 0 (Success)
+ */
+
+void postive_or_negative(int i)
+{
+	if (i < 0)
+	{
+		printf("%d is %s\n", i);
+	}
+	else if (i > 0)
+	{
+		pritnf("%d is %s\n", i);
+	}
+	else
+	{
+		pritnf("%d is %s\n", i);
+	}
+	return (0);
+}
