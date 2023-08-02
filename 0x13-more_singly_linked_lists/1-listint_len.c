8dc6fb7f422345d9d816aaf171b443b9b02dbea2
ChairmanBarrista <barristaamoah@gmail.com>
2023-08-02 08:38:47 -0700

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - a function that returns the number of
+ * elements in a linked listint_t list.
+ * @h: type listint_t to traverse
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
