0835dfa53bac27d885a4cdebff3355dcb99ebd5f
ChairmanBarrista <barristaamoah@gmail.com>
2023-08-02 09:34:10 -0700

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a listint_t list
+ * @head: listint_t list to be freed
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		head->next = tmp;
+		free(head);
+		tmp = head;
+	}
+}
